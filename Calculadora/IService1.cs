0dce5fcd488392588d3667c4d395863fb34f0b54
JohnLaptopVIII <JohnLaptopVIII@DESKTOP-CRMANNE>
2016-10-24 21:04:03 +0100

    Add project files.

---
+++ Calculadora/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        int Soma(int n1, int n2);
+
+        [OperationContract]
+        int Subtracao(int n1, int n2);
+
+        [OperationContract]
+        int Divisao(int n1, int n2);
+
+        [OperationContract]
+        int Multiplicacao(int n1, int n2);
+    }
+
+}
---
+++ Calculadora/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Subtracao(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public int Divisao(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+
+        public int Multiplicacao(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+    }
+
+}
